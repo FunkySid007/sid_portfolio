2cb5892bae7b402a09a6b73ba6450034609fb13b
Siddharth gautam <98460740+FunkySid007@users.noreply.github.com>
2023-10-01 00:37:54 +0530

    files

---
+++ script.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer.', 'Graphic Designer.', 'Web Designer.', 'Social Media Manager.', 'UI/UX Designer(Figma).'],
+    typeSpeed: 50
+  });
+  
